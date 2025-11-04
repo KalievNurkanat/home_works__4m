2d92984c4a561cdefa5ef8a01fe0459f408286af
KalievNurkanat <kalievnurkanat89@gmail.com>
2025-11-04 11:54:59 +0600

    hw2

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 """
 select * from posts; --> Post.objects.all()
 
@@ -14,6 +16,9 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=1000, null=True, blank=True)
     rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"{self.title} = {self.rate}"
