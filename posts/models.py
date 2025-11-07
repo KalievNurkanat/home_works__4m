e7f0d4eb6d4874f3998d3d062423f81fa639b695
KalievNurkanat <kalievnurkanat89@gmail.com>
2025-11-07 18:53:55 +0600

    hw

--- posts/models.py
+++ posts/models.py
@@ -11,14 +11,29 @@ select * from posts where title ILIKE '%test%'; ---> Post.objects.filter(title__
 """
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Post(models.Model):
+    image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=1000, null=True, blank=True)
     rate = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f"{self.title} = {self.rate}"
--- posts/views.py
+++ posts/views.py
@@ -15,3 +15,7 @@ def html_views(request):
 def post_list_view(request):
     posts = Post.objects.all()
     return render (request, "posts/post_list.html", context={"posts": posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "posts/post_detail.html", context={"post": post})
